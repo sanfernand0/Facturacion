edb4c8ec1383f420497b35efebf0ac39608ec82f
Luis Fernando <81476938+sanfernand0@users.noreply.github.com>
2021-05-20 08:41:53 -0600

    Update arqueov2.cs

--- Sistema/Sistema/Reportes/arqueov2.cs
+++ Sistema/Sistema/Reportes/arqueov2.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
-using Microsoft.Reporting.WinForms;
 using DGVPrinterHelper;
 namespace Sistema
 {
